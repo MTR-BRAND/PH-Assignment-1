286629ab0211d847827b105e420fbff0b549c859
MTR-BRAND <mahmudaarafat0077@gmail.com>
2025-11-17 20:59:09 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,89 @@
+Blog Post 1 : What are some differences between interfaces and types in TypeScript?
+
+TypeScript মূলত JavaScript এর উপর ভিত্তি করে তৈরি। এটি আমাদের variable, function, এবং object এর জন্য type নির্ধারণের সুযোগ দেয়। TypeScript-এ আমরা মূলত দুইভাবে object বা custom type define করতে পারি:
+
+i/Interface
+
+ii/Type Alias
+
+এদের কাজ প্রায় এক বলেই চলে কিন্তু দাড়াও এদের মধ্যে কিছু গুরুত্বপূর্ণ পার্থক্য আছে।
+
+১. Interface
+
+Definition: Interface মূলত একটি object এর structure define করার জন্য ব্যবহৃত হয়। এটি class এর সাথেও ব্যবহার করতে পারি।
+
+Extending / Inheritance: Interface সহজেই extend করতে পারি। একাধিক interface একসাথে extend করাও সম্ভব।
+
+Declaration Merging: একই নামে দুইটি interface declare করলে এগুলো automatic merge হয়ে যায়।
+
+Use case: Interface মূলত object বা class structure define করার জন্য ব্যবহৃত হয়।
+
+২. Type Alias
+
+Type alias দিয়ে primitive, union, tuple, function বা object সব ধরনের type তৈরি করতে পারি।
+
+Interface এর মতো extend করতে পারি না। তবে intersection (&) operator দিয়ে combine করা যায়।
+
+Declaration Merging সম্ভব না । একই নামের type পুনরায় declare করলে error দেবে।
+
+Type Alias অনেক বেশি flexible । Union type, tuple, function type ইত্যাদি define করতে পারি বা করা যায়।
+
+কোনটা কখন ব্যবহার করা উচিত?
+
+যদি object বা class structure define করতে চাই এবং inheritance বা merging দরকার হয় তাহলে Interface ব্যবহার করতে পারি।
+
+যদি union, tuple, function type বা advanced combination দরকার হয় তাহলে Type Alias ব্যবহার করতে পারি।
+
+
+
+
+
+
+
+
+Blog Post 2 : What is the use of the keyof keyword in TypeScript?
+
+keyof keyword TypeScript-এ ব্যবহার করা হয় কোনো type বা interface-এর সব property name বের করার জন্য।
+মানে, এটা object-এর key গুলোকে নিয়ে একটা union type বানায়।
+
+এটা খুব সহজ একটা Example দেখলেই বোঝা যাবে।
+
+Example => ধরো, ঝংকার ভাইয়ার একটা Interface আছে:
+
+interface Jhankar {
+    name: string;
+    age: number;
+    profession: string;
+    cseStudent: boolean;
+}
+
+
+keyof operator শুধুমাত্র Type Alias ও Interface এর সাথে কাজ করে।
+
+এখন আমাদের মূল টপিক =>
+
+ধরো, ঝংকার ভাইয়ার একজন বন্ধু আছে চাংকু।
+চাংকু ভাইও ঝংকার ভাইয়ের সাথে একই সাবজেক্টে পড়েছে।
+তাই তার প্রয়োজনীয় জিনিস (name, age, profession, cseStudent) সবই একই।
+
+এখন কি চাংকু ভাইয়ের জন্য নতুন করে আরেকটা আলাদা type বানাতে হবে?
+
+না, লাগবে না!
+কারণ চাংকু ভাইয়ের দরকারি সব property আগেই Jhankar interface এ আছে।
+
+আমরা শুধু =>
+
+type Chanku = keyof Jhankar;
+
+
+লিখলেই হয়ে গেল!
+
+এখন Chanku টাইপটি হবে:
+
+"name" | "age" | "profession" | "cseStudent"
+
+
+মানে—এটা শুধু key গুলো রাখবে, value না।
+ঝংকার ভাইয়ের টাইপ থেকে key বের করে চাংকু ভাইয়ের টাইপ তৈরি!
+
+আর আটাই বাস্তব হা হা হা.....
---
+++ solution.ts
@@ -0,0 +1,135 @@
+function formatValue(Input: string | number | boolean) {
+    if (typeof Input === 'string') {
+        return Input.toUpperCase()
+    }
+    if (typeof Input === 'number') {
+        return (Input * 10)
+    }
+    if (typeof Input === 'boolean') {
+        return (!Input)
+    }
+
+    const ex: never = Input
+    return ex;
+
+}
+
+
+
+
+
+
+
+function getLength(Input: string | unknown[]): number {
+    if (typeof Input === 'string') {
+        return Input.length
+    }
+    else if (Array.isArray(Input)) {
+        return Input.length
+    }
+
+
+    const ex: never = Input;
+    return ex;
+}
+
+
+
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getDetails(): string {
+        return `'Name: ${this.name}, Age: ${this.age}'`
+    }
+}
+
+
+
+
+
+function filterByRating(Input: { title: string, rating: number }[]) {
+    return Input.filter(item => (item.rating >= 4))
+}
+
+
+
+
+
+function filterActiveUsers(Input: { id: number, name: string, email: string, isActive: boolean }[]): { id: number, name: string, email: string, isActive: boolean }[] {
+    return Input.filter(item => (item.isActive === true))
+}
+
+
+
+
+
+
+interface Book {
+    title: string
+    author: string
+    publishedYear: number
+    isAvailable: boolean
+}
+
+function printBookDetails(Input: Book) {
+    console.log(`Title: ${Input.title}, Author: ${Input.author}, Published: ${Input.publishedYear}, Available: ${Input.isAvailable ? "Yes" : "No"}`);
+}
+
+
+
+
+
+
+
+function getUniqueValues<T extends string | number>(array1: T[], array2: T[]) {
+    const result: T[] = []
+
+    for (let item of array1) {
+        if (!result.includes(item)) {
+            result.push(item);
+        }
+    }
+
+    for (let item of array2) {
+        if (!result.includes(item)) {
+            result.push(item);
+        }
+    }
+    return result;
+}
+
+
+
+
+
+
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+}
+
+
+function calculateTotalPrice(Input: Product[]){
+
+    return Input.reduce((total , product)=>{
+        console.log(total,product);
+        if(product.discount !== undefined && (product.discount < 0 || product.discount > 100)){
+            throw new Error(`Discount for "${product.name}" must be between 0 and 100`);
+        }
+
+        const productTotal = product.price * product.quantity;
+        const discountedTotal = product.discount ? productTotal * (1 - product.discount / 100) : productTotal;
+
+        return total + discountedTotal
+    } , 0)
+
+}
